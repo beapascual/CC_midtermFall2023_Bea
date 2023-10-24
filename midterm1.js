d8ef86589a2195d28c00607bf93482debe0b8169
beapascual <btp6195@nyu.edu>
2023-10-23 23:13:31 -0400

    attempt at ripple scene (kinda worked?)

--- midterm1.js
+++ midterm1.js
@@ -2,15 +2,22 @@ let option = 1;
 let tall = [];
 let city = [];
 let c1, c2, c3, c4;
+let circleX;
+let circleY;
+let circleSize;
+//let circleStatus = false;
 
 function setup() {
   createCanvas(800, 800);
   background(255);
-  
-    c1= color(157, 94, 156);
-c2 = color(251, 233, 113);
+
+  c1 = color(157, 94, 156);
+  c2 = color(251, 233, 113);
   c3 = color(29, 53, 87);
- c4 = color(168, 218, 220);
+  c4 = color(168, 218, 220);
+  circleX = width / 2;
+  circleY = height / 2;
+  circleSize = 0;
 
   for (i = 0; i < 8; i++) {
     tall[i] = random(300, 550);
@@ -18,7 +25,7 @@ c2 = color(251, 233, 113);
   for (i = 0; i < 8; i++) {
     city[i] = new building(i * 100, height - tall[i], 100, tall[i], c3);
   }
-  
+
   // for (i = 0; i < 3; i ++){
   //   for (j = 0; j < 8; i++){
   //     city[i][j] = new building(i * 100, height - tall[i], 100, tall[i], c3);
@@ -40,10 +47,26 @@ function setGradient(c1, c2) {
 function draw() {
   if (option === 1) {
     background(0);
+    noFill();
+    strokeWeight(5);
+    stroke(255);
+
+    circleSize += 10;
+
+    stroke(0, 64, 128);
+    circle(circleX, circleY, circleSize);
+    circle(circleX, circleY, circleSize * 0.75);
+    circle(circleX, circleY, circleSize * 0.5);
   } // ripple scene
+  
+  function mousePressed() {
+    circleX = mouseX;
+  circleY = mouseY;
+  circleSize = 0;
+  }
 
   if (option === 2) {
-    setGradient(c1,c2);
+    setGradient(c1, c2);
   } // sunset scene
 
   if (option === 3) {
@@ -67,4 +90,3 @@ function keyPressed() {
     option = 1;
   }
 }
-
