e69a0c66c8b522f4f2c58d64f6fd832ea6441f3f
beapascual <btp6195@nyu.edu>
2023-10-25 15:33:56 -0400

    combined city/sunset gradient,, still won't run in vsc, switching to scene 4 is weird?

--- midterm1.js
+++ midterm1.js
@@ -7,53 +7,42 @@ let city = [];
 let city2 = [];
 let city3 = []; // for city
 
-
 let c1, c2, c3, c4, c5; // colors
-
 
 let circleX;
 let circleY;
 let circleSize; // for ripples
 
-
 function setup() {
   createCanvas(800, 800);
   background(255);
 
-  c1 = color(157, 94, 156);
-  c2 = color(251, 233, 113); // for gradient
-
-  c3 = color(32, 31, 70);
-  c4 = color(45, 43, 111);
-  c5 = color(55, 55, 149); // for city
+  c1 = color(0, 0, 153);
+  c2 = color(204, 51, 0); 
+  c3 = color(32, 31, 70, 100);
+  c4 = color(54, 40, 78);
+  c5 = color(55, 55, 149);
 
   circleX = width / 2;
   circleY = height / 2;
   circleSize = 0; // for ripples
 
-
-    for (i = 0; i < 9; i++) {
-    tall[i] = random(575, 650);
-    tall2[i] = random (475, 550);
-    tall3[i]= random (300,450);
-  } // heights of buildings (city)
+  for (i = 0; i < 9; i++) {
+    tall[i] = random(425, 500);
+    tall2[i] = random(325, 400);
+    tall3[i] = random(200, 300);
+  }
   for (i = 0; i < 9; i++) {
     city3[i] = new building(
       i * 100 - 100,
-      height - tall3[i] ,
+      height - tall3[i],
       100,
       tall3[i],
       c5
     );
-    city2[i] = new building(
-      i * 100 - 50,
-      height - tall2[i],
-      100,
-      tall2[i],
-      c4
-    );
+    city2[i] = new building(i * 100 - 50, height - tall2[i], 100, tall2[i], c4);
     city[i] = new building(i * 100 - 30, height - tall[i], 100, tall[i], c3);
-  } // placements of buildings (city)
+  }
 }
 
 function setGradient(c1, c2) {
@@ -64,7 +53,7 @@ function setGradient(c1, c2) {
     stroke(c);
     line(0, y, width, y);
   }
-} // gradient background of sunset
+} // night sky gradient of city
 
 function draw() {
   if (option === 1) {
@@ -87,28 +76,32 @@ function draw() {
   } // ripples
 
   if (option === 2) {
-    setGradient(c1, c2);
-  } // sunset scene
-
-  if (option === 3) {
-    background(2, 16, 89);
-    stroke(0);
+    frameRate(1);
     strokeWeight(1);
-  for (i = 0; i < 9; i++) {
-    city[i].display();
-    city2[i].display();
-    city3[i].display();
+    setGradient(c1, c2);
+    for (var j = 0; j < 50; j++) {
+      var x = random(width);
+      var y = random(height - 500);
+      noStroke();
+      fill(255, 255, 0);
+      ellipse(x, y, 3, 3);
     }
-  } //night city
+    for (i = 0; i < 9; i++) {
+      stroke(1);
+      city[i].display();
+      city2[i].display();
+      city3[i].display();
+    }
+  }
+} //night city
 
-  if (option === 4) {
-    background(178, 190, 136, 150);
-  } // breathing
-}
+if (option === 3) {
+  background(178, 190, 136, 150);
+} // breathing
 
 function keyPressed() {
   option++;
-  if (option > 4) {
+  if (option > 3) {
     option = 1;
   }
 } // to change scenes
