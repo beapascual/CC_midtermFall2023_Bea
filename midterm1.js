dccf7d280e9de54a72ad16a77b1f012ba3094575
beapascual <btp6195@nyu.edu>
2023-10-24 23:17:08 -0400

    staggered building placement and height,, still only works in web editor

--- midterm1.js
+++ midterm1.js
@@ -1,5 +1,7 @@
 let option = 1;
 let tall = [];
+let tall2 = [];
+let tall3 = [];
 let city = [];
 let city2 = [];
 let city3 = [];
@@ -22,25 +24,27 @@ function setup() {
   circleY = height / 2;
   circleSize = 0;
 
-  for (i = 0; i < 8; i++) {
-    tall[i] = random(300, 550);
+    for (i = 0; i < 9; i++) {
+    tall[i] = random(575, 650);
+    tall2[i] = random (475, 550);
+    tall3[i]= random (300,450);
   }
-  for (i = 0; i < 8; i++) {
+  for (i = 0; i < 9; i++) {
     city3[i] = new building(
-      i * 100,
-      height - (tall[i] - 150),
+      i * 100 - 100,
+      height - tall3[i] ,
       100,
-      tall[i] - 150,
+      tall3[i],
       c5
     );
     city2[i] = new building(
-      i * 100,
-      height - (tall[i] - 75),
+      i * 100 - 50,
+      height - tall2[i],
       100,
-      tall[i] - 75,
+      tall2[i],
       c4
     );
-    city[i] = new building(i * 100, height - tall[i], 100, tall[i], c3);
+    city[i] = new building(i * 100 - 30, height - tall[i], 100, tall[i], c3);
   }
 }
 
@@ -80,14 +84,13 @@ function draw() {
   } // sunset scene
 
   if (option === 3) {
+    background(2, 16, 89);
     stroke(0);
-    strokeWeight(3);
-    background(2, 16, 89);
-    background(2, 16, 89);
-    for (i = 0; i < 8; i++) {
-      city[i].display();
-      city2[i].display();
-      city3[i].display();
+    strokeWeight(1);
+  for (i = 0; i < 9; i++) {
+    city[i].display();
+    city2[i].display();
+    city3[i].display();
     }
   } //night sky
 
