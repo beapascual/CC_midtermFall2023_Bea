d40e9e3e9c129ea57887e5501b5dd0d02a3aafb2
beapascual <btp6195@nyu.edu>
2023-10-27 01:50:49 -0400

    FIXED BUILDING FILE!! IT RUNS IN BROWSER YAY

--- building.js
+++ building.js
@@ -1,27 +1,20 @@
-let s;
+let i;
 let j;
 
 class building{
-  constructor(x, y, w, h){
+  constructor(x, y, w, h, c){
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.c = c;
   }
   
   display(){
-    fill(0);
+    fill(this.c);
     rect(this.x,this.y,this.w,this.h);
-    fill(255,255,0)
-    for(s=0; s< this.w; s = this.x + 10){
-      for(j=0; j<this.h; j = this.y + 10){
-        rect(this.x,this.y,20,20);
   }
 }
-  }
-}
-
-
 
 
 
