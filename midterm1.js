be99f48bfd66958cb73bd879e098e708858dcc14
beapascual <btp6195@nyu.edu>
2023-10-30 14:38:00 -0400

    tidied code

--- midterm1.js
+++ midterm1.js
@@ -13,9 +13,7 @@ let circleX;
 let circleY;
 let circleSize; // for ripples
 
-let angle = 0 // for breathing circle
-
-
+let angle = 0; // for breathing circle
 
 function setup() {
   createCanvas(800, 800);
@@ -63,7 +61,6 @@ function setGradient(c1, c2) {
 function draw() {
   if (option === 1) {
     background(0);
-    
 
     if (mouseIsPressed === true) {
       circleX = mouseX;
@@ -92,14 +89,13 @@ function draw() {
       noStroke();
       fill(255, 255, 0);
       ellipse(x, y, 3, 3);
-    }// stars https://codeburst.io/sunsets-and-shooting-stars-in-p5-js-92244d238e2b
+    } // stars https://codeburst.io/sunsets-and-shooting-stars-in-p5-js-92244d238e2b
     for (i = 0; i < 9; i++) {
       stroke(1);
       city[i].display();
       city2[i].display();
       city3[i].display();
     }
-    
   }
   //night city
 
@@ -110,30 +106,29 @@ function draw() {
     noStroke();
     background(178, 190, 136, 150);
 
-const from = color(210,245,158);
-const to = color(255);
-colorMode(RGB);
-const interA = lerpColor(from, to, 0.33);
-const interB = lerpColor(from, to, 0.66);
-fill(from);
-ellipse(0,0,400);
-fill(interA);
-ellipse(0,0,300);
-fill(interB);
-ellipse(0,0,200);
-fill(to);
-ellipse(0,0,100); //  (references)
-  
+    const from = color(210, 245, 158);
+    const to = color(255);
+    colorMode(RGB);
+    const interA = lerpColor(from, to, 0.33);
+    const interB = lerpColor(from, to, 0.66);
+    fill(from);
+    ellipse(0, 0, 400);
+    fill(interA);
+    ellipse(0, 0, 300);
+    fill(interB);
+    ellipse(0, 0, 200);
+    fill(to);
+    ellipse(0, 0, 100); //  (references)
 
-const r = map(sin(angle), -1, 1, 0, 100);
-    const baseRadius = 400
-  
+    const r = map(sin(angle), -1, 1, 0, 100);
+    const baseRadius = 400;
+
     fill(26, 113, 63, 150);
     circle(0, 0, map(r, 0, 100, 0, baseRadius));
-  
+
     angle += 0.012;
-} // breathing circle https://editor.p5js.org/dansakamoto/sketches/H1ICcXXtm
-} 
+  } // breathing circle https://editor.p5js.org/dansakamoto/sketches/H1ICcXXtm (kinda)
+}
 
 function keyPressed() {
   option++;
